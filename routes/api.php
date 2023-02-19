2ebbd050139921cb228ff1a055e0a247e0226860
C0t4n1qu1l4t0r <daw06.2022@gmail.com>
2023-02-20 00:29:40 +0100

    Spoticoty

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PlaylistController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\SongController;
@@ -22,6 +23,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::get('me', 'me');
+});
+
 
 Route::controller(UserController::class)->group(function(){
     Route::post('register', 'register');
